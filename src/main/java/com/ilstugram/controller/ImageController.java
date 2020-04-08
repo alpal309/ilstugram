2834524632401011c8672eae13e892ec6b8c2a46
Alan Hunt <aphunt92@gmail.com>
2020-04-07 19:40:39 -0500

    small tweaks

--- src/main/java/com/ilstugram/controller/ImageController.java
+++ src/main/java/com/ilstugram/controller/ImageController.java
@@ -19,7 +19,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Objects;
